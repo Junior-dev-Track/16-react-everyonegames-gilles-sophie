7693e62ef6fecfcd1233642bb779948f774d44ea
Kriidfel <kriidfel@Kriidfel>
2024-04-26 16:00:08 +0200

    Updating api

---
+++ src/components/Card.jsx
@@ -0,0 +1,44 @@
+import Api from "./Api.js";
+import {useState} from "react";
+
+export default function Card() {
+
+    const api = new Api();
+
+    const [card, setCard] = useState("")
+
+    const popularMovies =  api.getPopularMovies("fr-FR",1)
+    console.log(popularMovies)
+    
+
+
+
+
+    return (
+        <div className="card">
+            <div className="card__img">
+                The img of the film
+            </div>
+            <div className="card__title">
+                A title
+            </div>
+            <div className="card__hover__wrap">
+                <div className="card__info">
+                    <span className={"rating"}>
+                    </span>
+                    <span className={"age"}>
+                    </span>
+                    <span className={"date"}>
+                    </span>
+                    <span className={"duree"}>
+                    </span>
+                </div>
+                <div className="card__tags">
+                    <span>
+
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
